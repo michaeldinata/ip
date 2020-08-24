d745b42283679eeb67714278431e6ad9f0c9de7b
Michael Dinata <michaeldinata98@gmail.com>
2020-08-24 19:57:17 +0800

    added level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,12 +10,28 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         greet();
+        echo();;
+        bye();
     }
 
     public static void greet(){
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println();
+    }
+
+    public static void echo(){
+        Scanner in = new Scanner(System.in);
+
+        while(true){
+            String command = in.nextLine();
+            if(command.compareTo("bye") == 0){
+                return;
+            }
+            System.out.println(command);
+        }
+    }
+
+    public static void bye(){
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
