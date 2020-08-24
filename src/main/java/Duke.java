e8f456a10b892d37fd0177afbcd81d9d0d555330
Michael Dinata <michaeldinata98@gmail.com>
2020-08-25 00:31:28 +0800

    Added Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,32 @@
 import java.util.*;
 
 public class Duke {
+    public static class Task{
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description){
+            this.description = description;
+            this.isDone = false;
+        }
+    
+        public String getStatusIcon(){
+            return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        }
+    
+        public String getDescription(){
+            return description;
+        }
+        
+        public void setDescription(String description){
+            this.description = description;
+        }
+    
+        public void markAsDone(){
+            this.isDone = true;
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,7 +36,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         greet();
-        GetCommand();
+        GetTask();
         bye();
     }
 
@@ -19,10 +45,10 @@ public class Duke {
         System.out.println("What can I do for you?");
     }
 
-    public static void GetCommand(){
-        Vector<String> ListToDo = new Vector<String>();
+    public static void GetTask(){
+        Task[] ListToDo = new Task[100];
         Scanner in = new Scanner(System.in);
-        int currentCommandNumber = 0;
+        int currentTaskCount = 0;
 
         while(true){
             String command = in.nextLine();
@@ -30,19 +56,32 @@ public class Duke {
                 return;
             }
             else if(command.compareTo("list") == 0){
-                list(ListToDo);
+                listOutTasks(ListToDo, currentTaskCount);
             }
+            else if(command.contains("done")){
+                int indexTaskDone = Integer.parseInt(command.substring(5, command.length()));
+                Task currentTask = ListToDo[indexTaskDone-1];
+                currentTask.markAsDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("[" + currentTask.getStatusIcon() + "] " + currentTask.getDescription());
+                System.out.println();
+            }
+
             else{
-                ListToDo.add(command);
+                Task newTask = new Task(command);
+                ListToDo[currentTaskCount++] = newTask;
                 System.out.println("Added: " + command);
+                System.out.println();
             }
         }
     }
 
-    public static void list(Vector ListToDo){
-        for(int i = 0; i < ListToDo.size(); i++){
-            System.out.println((i+1) + ". " + ListToDo.get(i));
+    public static void listOutTasks(Task[] ListToDo, int currentTaskCount){
+        for(int i = 0; i < currentTaskCount; i++){
+            Task currentTask = ListToDo[i];
+            System.out.println((i+1) + ".[" + currentTask.getStatusIcon() + "] " + currentTask.getDescription());
         }
+        System.out.println();
     }
 
     public static void bye(){
