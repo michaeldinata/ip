28bf97be7b9ffc0647a45b35c9343f1753ed2b2e
Michael Dinata <michaeldinata98@gmail.com>
2020-09-03 17:51:26 +0800

    changed coding standard and added extra UX

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,11 @@
 import java.util.*;
 
 public class Duke {
-    public static class Task{
-        protected String description;
-        protected boolean isDone;
 
-        public Task(String description){
-            this.description = description;
-            this.isDone = false;
-        }
-    
-        public String getStatusIcon(){
-            return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
-        }
-    
-        public String getDescription(){
-            return description;
-        }
-        
-        public void setDescription(String description){
-            this.description = description;
-        }
-    
-        public void markAsDone(){
-            this.isDone = true;
-        }
-    }
+    public static final String separatingLine = "  ______________________________";
+    private static final int MAX_TASKS = 100;
+    public static int currentTaskCount = 0;
+    static Task[] tasksToDo = new Task[MAX_TASKS];
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -41,46 +21,56 @@ public class Duke {
     }
 
     public static void greet(){
+        System.out.println(separatingLine);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        System.out.println(separatingLine);
     }
 
     public static void GetTask(){
-        Task[] ListToDo = new Task[100];
         Scanner in = new Scanner(System.in);
-        int currentTaskCount = 0;
 
         while(true){
             String command = in.nextLine();
-            if(command.compareTo("bye") == 0){
-                return;
-            }
-            else if(command.compareTo("list") == 0){
-                listOutTasks(ListToDo, currentTaskCount);
-            }
-            else if(command.contains("done")){
-                int indexTaskDone = Integer.parseInt(command.substring(5, command.length()));
-                Task currentTask = ListToDo[indexTaskDone-1];
-                currentTask.markAsDone();
-                System.out.println("Nice! I've marked this task as done: ");
-                System.out.println("[" + currentTask.getStatusIcon() + "] " + currentTask.getDescription());
-                System.out.println();
-            }
 
-            else{
-                Task newTask = new Task(command);
-                ListToDo[currentTaskCount++] = newTask;
-                System.out.println("Added: " + command);
-                System.out.println();
+            if(command.equals("bye")){
+                return;
+            } else if(command.equals("list")){
+                listOutTasks();
+            } else if(command.contains("done")){
+                markAsDone(command);
+            } else{
+                addNewTask(command);
             }
         }
     }
 
-    public static void listOutTasks(Task[] ListToDo, int currentTaskCount){
+    public static void listOutTasks(){
+        System.out.println(separatingLine);
         for(int i = 0; i < currentTaskCount; i++){
-            Task currentTask = ListToDo[i];
-            System.out.println((i+1) + ".[" + currentTask.getStatusIcon() + "] " + currentTask.getDescription());
+            Task currentTask = tasksToDo[i];
+            System.out.println("    " + (i+1) + ".[" + currentTask.getStatusIcon() + "] " 
+            + currentTask.getDescription());
         }
+        System.out.println(separatingLine);
+        System.out.println();
+    }
+
+    public static void markAsDone(String command){
+        int indexTaskDone = Integer.parseInt(command.substring(5, command.length()));
+        Task currentTask = tasksToDo[indexTaskDone-1];
+        System.out.println(separatingLine);
+        currentTask.markAsDone();
+        System.out.println(separatingLine);
+        System.out.println();
+    }
+
+    public static void addNewTask(String command){
+        Task newTask = new Task(command);
+        tasksToDo[currentTaskCount++] = newTask;
+        System.out.println();
+        System.out.println("    Added: " + command);
+        System.out.println(separatingLine);
         System.out.println();
     }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+
+
+public class Task{
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description){
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon(){
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public String getDescription(){
+        return description;
+    }
+    
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void markAsDone(){
+        this.isDone = true;
+        System.out.println("    Nice! I've marked this task as done: ");
+        System.out.println("    [" + this.getStatusIcon() + "] " + this.getDescription());
+    }
+}
